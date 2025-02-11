716a6db8f18ceafb756609ce763522130a05e555
Aryan Kesarkar <110324167+aryannprog@users.noreply.github.com>
2025-02-12 00:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,6 @@ def fetch_nykaa_price(url):
     options.add_argument("--headless=new")  # Run in headless mode (no browser window)
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    options.add_argument("--window-size=1920x1080")  # Set a realistic screen size
-    options.add_argument("--disable-gpu")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")  # Spoof user-agent
 
     # Initialize WebDriver
     service = Service("/usr/bin/chromedriver")  # Set manual Chromedriver path
@@ -230,10 +227,7 @@ def fetch_blinkit_price(url):
     options.add_argument("--headless=new")  # Run in headless mode (no browser window)
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    options.add_argument("--window-size=1920x1080")  # Set a realistic screen size
-    options.add_argument("--disable-gpu")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")  # Spoof user-agent
-
+    
     # Initialize WebDriver
     service = Service("/usr/bin/chromedriver")  # Set manual Chromedriver path
     driver = webdriver.Chrome(service=service, options=options)
