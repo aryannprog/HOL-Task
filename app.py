bb6d1bfa49b05b360b0e358a36cee8bbb1bd4cfc
Aryan Kesarkar <110324167+aryannprog@users.noreply.github.com>
2025-02-12 21:24:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def fetch_amazon_price(url):
 
     try:
         driver.get(url)
-        time.sleep(10)  # Wait for the page to load
+        time.sleep(20)  # Wait for the page to load
 
         # Check for CAPTCHA
         if "Enter the characters" in driver.page_source:
